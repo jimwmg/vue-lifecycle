2ee593af7eced8a4922c321f4edbf055467b44a7
jimwmg <550116990@qq.com>
2018-02-09 14:07:58 +0800

    in mi s teinmiste

---
+++ src/components/inmiste.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+         i am inmiste
+    </div>
+</template>
+<script>
+    export default {
+        beforeCreate(){
+            console.log('inMiste.Vue BeforeCreate')
+        },
+        created(){
+            console.log('inMiste.Vue Created')
+        },
+        beforeMount(){
+            console.log('inMiste.vue beforeMounted')
+        },
+        mounted(){
+            console.log('inMiste.vue mounted')
+        },
+        activated(){
+            console.log('inMiste.vue actived')
+        },
+        deactivated(){
+            console.log('inMiste.vue deactived')
+        },
+        beforeDestroy(){
+            console.log('inMiste.vue beforeDestroy')
+        },
+        beforeUpdate(){
+            console.log('inMiste.vue beforeupdate')
+        },
+        updated(){
+            console.log('inMiste.vue update')
+        },
+        destroyed(){
+            console.log('inMiste.vue destroyed')
+        }
+    }
+</script>
